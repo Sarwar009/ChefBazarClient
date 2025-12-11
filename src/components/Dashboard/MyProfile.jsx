3bf1f03699006448a007177d4598de7c0ce0f897
Sarwar009 <ssarwar459@gmail.com>
2025-12-11 19:13:11 +0600

    update profile done

--- src/components/Dashboard/MyProfile.jsx
+++ src/components/Dashboard/MyProfile.jsx
@@ -1,11 +1,14 @@
-
 import React from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
 export default function MyProfile() {
+  const {user, role} = useAuth();
+  const navigate = useNavigate();
 
-    const navigate =useNavigate()
+  console.log(user);
+  
   return (
     <motion.div
       initial={{ opacity: 0, y: 18 }}
@@ -13,30 +16,32 @@ export default function MyProfile() {
       transition={{ duration: 0.45 }}
       className="bg-white rounded-2xl shadow-md p-6"
     >
-      <h2 className="text-2xl font-bold mb-4">My Profile</h2>
+      <h2 className="text-2xl font-bold mb-4 text-center">My Profile</h2>
 
-      <div className="flex gap-6 items-center">
+      <div className="flex flex-col gap-6 items-center justify-center">
         <div className="w-28 h-28 rounded-full bg-gray-100 overflow-hidden flex-shrink-0">
-          <img src="https://i.ibb.co/7W7T9dP/spicy-chicken.jpg" alt="avatar" className="w-full h-full object-cover"/>
+          <img
+            src={user.photoURL}
+            alt="avatar"
+            className="w-full h-full object-cover"
+          />
         </div>
 
-        <div>
-          <p className="text-lg font-semibold">User Name</p>
-          <p className="text-sm text-gray-500">user@example.com</p>
-          <p className="mt-3 text-sm text-gray-600">
-            Role: <span className="font-medium">User</span>
+        <div className="flex flex-col mx-1 md:mx-4">
+          <p className="text-lg">Name: <span className=" font-semibold">{user.displayName}</span></p>
+          <p className="text-sm">Email: <span className=" font-semibold">{user.email}</span></p>
+          <p className="mt-3 text-sm">
+            Role: <span className="font-medium">{role}</span>
           </p>
         </div>
       </div>
 
-      <div className="mt-6 space-x-3">
-        <button 
-        onClick={()=> navigate('/dashboard/update-profile')}
-        className="px-4 py-2 bg-emerald-500 text-white rounded-lg shadow hover:scale-105 transition">
+      <div className="mt-6 space-x-3 text-center">
+        <button
+          onClick={() => navigate("/dashboard/update-profile")}
+          className="px-4 py-2 bg-emerald-500 text-white rounded-lg shadow hover:scale-105 transition cursor-pointer"
+        >
           Edit Profile
-        </button>
-        <button className="px-4 py-2 border rounded-lg hover:bg-gray-50 transition">
-          Change Password
         </button>
       </div>
     </motion.div>
